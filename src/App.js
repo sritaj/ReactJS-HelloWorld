da7da7e9c1023f73b2927fb6e9d22fcbe4051d59
Sritaj <sritajp@gmail.com>
2021-05-11 23:44:48 +0530

    Added App.js and structured code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Hello World</h1>
+      <button className="button">Hello World</button>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style.css";
-
-function App() {
-  return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Hello World</h1>
-      <button className="button">Heyyy</button>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
